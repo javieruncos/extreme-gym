a1665c4a9c317252342f31fa1688e6bdffa38ab0
Javier Uncos <javiersrfc30@gmail.com>
2024-04-16 21:16:22 -0300

    agregamos maquetado y estilos a la pagina de Horarios

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Inicio from './components/pages/Inicio';
 import Formacion from './components/pages/Formacion';
 import Menu from './components/common/Menu';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Horarios from './components/pages/Horarios';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Inicio />} />
           <Route path='/Formacion' element={<Formacion/>} />
+          <Route path='/Horarios' element={<Horarios/>} />
         </Routes>
         </BrowserRouter>
       </div>
